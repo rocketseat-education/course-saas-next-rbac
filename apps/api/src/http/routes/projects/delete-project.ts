24853f311deca46bb2edea52ce5e463ba1c32e42
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-04-22 17:01:36 -0300

    Desenvolvendo o back-end: Rota: Remover projeto

---
+++ apps/api/src/http/routes/projects/delete-project.ts
@@ -0,0 +1,67 @@
+import { projectSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { BadRequestError } from '@/http/routes/_errors/bad-request-error'
+import { UnauthorizedError } from '@/http/routes/_errors/unauthorized-error'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+export async function deleteProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:slug/projects/:projectId',
+      {
+        schema: {
+          tags: ['Projects'],
+          summary: 'Delete a project',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            projectId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, projectId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const project = await prisma.project.findUnique({
+          where: {
+            id: projectId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!project) {
+          throw new BadRequestError('Project not found.')
+        }
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+        const authProject = projectSchema.parse(project)
+
+        if (cannot('delete', authProject)) {
+          throw new UnauthorizedError(
+            `You're not allowed to delete this project.`,
+          )
+        }
+
+        await prisma.project.delete({
+          where: {
+            id: projectId,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -25,6 +25,7 @@ import { shutdownOrganization } from '@/http/routes/orgs/shutdown-organization'
 import { transferOrganization } from '@/http/routes/orgs/transfer-organization'
 import { updateOrganization } from '@/http/routes/orgs/update-organization'
 import { createProject } from '@/http/routes/projects/create-project'
+import { deleteProject } from '@/http/routes/projects/delete-project'
 
 import { createAccount } from './routes/auth/create-account'
 
@@ -81,6 +82,7 @@ app.register(shutdownOrganization)
 app.register(transferOrganization)
 
 app.register(createProject)
+app.register(deleteProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
