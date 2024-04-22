0c32beb648507ff3843a397bbc2c75123ff7568d
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-04-22 16:45:07 -0300

    Desenvolvendo o back-end: Rota: Transferir organização

---
+++ apps/api/src/http/routes/orgs/transfer-organization.ts
@@ -0,0 +1,88 @@
+import { organizationSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { BadRequestError } from '@/http/routes/_errors/bad-request-error'
+import { UnauthorizedError } from '@/http/routes/_errors/unauthorized-error'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+export async function transferOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .patch(
+      '/organizations/:slug/owner',
+      {
+        schema: {
+          tags: ['Organizations'],
+          summary: 'Transfer organization ownership',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            transferToUserId: z.string().uuid(),
+          }),
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const authOrganization = organizationSchema.parse(organization)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('transfer_ownership', authOrganization)) {
+          throw new UnauthorizedError(
+            `You're not allowed to transfer this organization ownership.`,
+          )
+        }
+
+        const { transferToUserId } = request.body
+
+        const transferMembership = await prisma.member.findUnique({
+          where: {
+            organizationId_userId: {
+              organizationId: organization.id,
+              userId: transferToUserId,
+            },
+          },
+        })
+
+        if (!transferMembership) {
+          throw new BadRequestError(
+            'Target user is not a member of this organization.',
+          )
+        }
+
+        await prisma.$transaction([
+          prisma.member.update({
+            where: {
+              organizationId_userId: {
+                organizationId: organization.id,
+                userId: transferToUserId,
+              },
+            },
+            data: {
+              role: 'ADMIN',
+            },
+          }),
+          prisma.organization.update({
+            where: { id: organization.id },
+            data: { ownerId: transferToUserId },
+          }),
+        ])
+
+        return reply.status(204).send()
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { getMembership } from '@/http/routes/orgs/get-membership'
 import { getOrganization } from '@/http/routes/orgs/get-organization'
 import { getOrganizations } from '@/http/routes/orgs/get-organizations'
 import { shutdownOrganization } from '@/http/routes/orgs/shutdown-organization'
+import { transferOrganization } from '@/http/routes/orgs/transfer-organization'
 import { updateOrganization } from '@/http/routes/orgs/update-organization'
 
 import { createAccount } from './routes/auth/create-account'
@@ -76,6 +77,7 @@ app.register(getOrganization)
 app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
+app.register(transferOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
