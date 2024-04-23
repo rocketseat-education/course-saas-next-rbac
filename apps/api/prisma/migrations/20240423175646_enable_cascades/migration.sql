d5b500473d7898f1624f0b9755ab747abf6ae9a8
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-04-23 14:57:00 -0300

    Desenvolvendo o back-end: Ajustes nos cascades do banco de dados

---
+++ apps/api/prisma/migrations/20240423175646_enable_cascades/migration.sql
@@ -0,0 +1,35 @@
+-- DropForeignKey
+ALTER TABLE "accounts" DROP CONSTRAINT "accounts_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "invites" DROP CONSTRAINT "invites_organization_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "members" DROP CONSTRAINT "members_organization_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "members" DROP CONSTRAINT "members_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "projects" DROP CONSTRAINT "projects_organization_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "tokens" DROP CONSTRAINT "tokens_user_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "tokens" ADD CONSTRAINT "tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "invites" ADD CONSTRAINT "invites_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "members" ADD CONSTRAINT "members_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "members" ADD CONSTRAINT "members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "projects" ADD CONSTRAINT "projects_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -35,14 +35,21 @@ export async function resetPassword(app: FastifyInstance) {
 
       const passwordHash = await hash(password, 6)
 
-      await prisma.user.update({
-        where: {
-          id: tokenFromCode.userId,
-        },
-        data: {
-          passwordHash,
-        },
-      })
+      await prisma.$transaction([
+        prisma.user.update({
+          where: {
+            id: tokenFromCode.userId,
+          },
+          data: {
+            passwordHash,
+          },
+        }),
+        prisma.token.delete({
+          where: {
+            id: code,
+          },
+        }),
+      ])
 
       return reply.status(204).send()
     },
