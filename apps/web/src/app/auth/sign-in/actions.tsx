9a700a6692d32614cc84f5d6d14928464d7a4e4f
Vini Fraga <vinifragam@gmail.com>
2024-06-04 12:43:38 -0300

    Desenvolvendo o front-end - Setup do cliente HTTP (ky)

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,5 +1,14 @@
 'use server'
 
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
 export async function signInWithEmailAndPassword(data: FormData) {
-  console.log(Object.fromEntries(data))
+  const { email, password } = Object.fromEntries(data)
+
+  const result = await signInWithPassword({
+    email: String(email),
+    password: String(password),
+  })
+
+  console.log(result)
 }
