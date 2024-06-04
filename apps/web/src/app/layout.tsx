49267bc631b1da20753653ee94554882e1054e3f
Vini Fraga <vinifragam@gmail.com>
2024-06-04 11:18:33 -0300

    Desenvolvendo o front-end - Setup do projeto Next.js

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Create Next App',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
