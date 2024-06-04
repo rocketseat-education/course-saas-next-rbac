3dd860efa22645ad51e1a0fcc69d8826897307f2
Vini Fraga <vinifragam@gmail.com>
2024-06-04 13:02:31 -0300

    Desenvolvendo o front-end - Usando hook useActionState

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -2,8 +2,15 @@
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
-export async function signInWithEmailAndPassword(data: FormData) {
+export async function signInWithEmailAndPassword(
+  previousState: unknown,
+  data: FormData,
+) {
+  console.log(previousState)
+
   const { email, password } = Object.fromEntries(data)
+
+  await new Promise((resolve) => setTimeout(resolve, 2000))
 
   const result = await signInWithPassword({
     email: String(email),
@@ -11,4 +18,6 @@ export async function signInWithEmailAndPassword(data: FormData) {
   })
 
   console.log(result)
+
+  return 'Sucesso!'
 }
---
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { Loader2 } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useActionState } from 'react'
+
+import githubIcon from '@/assets/github-icon.svg'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+import { signInWithEmailAndPassword } from './actions'
+
+export function SignInForm() {
+  const [state, formAction, isPending] = useActionState(
+    signInWithEmailAndPassword,
+    null,
+  )
+
+  return (
+    <form action={formAction} className="space-y-4">
+      <h1>{state}</h1>
+
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+
+        <Link
+          href="/auth/forgot-password"
+          className="text-xs font-medium text-foreground hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+
+      <Button className="w-full" type="submit" disabled={isPending}>
+        {isPending ? (
+          <Loader2 className="size-4 animate-spin" />
+        ) : (
+          'Sign in with e-mail'
+        )}
+      </Button>
+
+      <Button className="w-full" variant="link" size="sm" asChild>
+        <Link href="/auth/sign-up">Create new account</Link>
+      </Button>
+
+      <Separator />
+
+      <Button type="submit" className="w-full" variant="outline">
+        <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
+        Sign in with GitHub
+      </Button>
+    </form>
+  )
+}
