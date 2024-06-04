63f8263cdac80e6df41bb3c08dc3d8eb1a855b86
Vini Fraga <vinifragam@gmail.com>
2024-06-04 16:48:07 -0300

    Desenvolvendo o front-end - Criando hook useFormState

---
+++ apps/web/src/hooks/use-form-state.ts
@@ -0,0 +1,33 @@
+import { FormEvent, useState, useTransition } from 'react'
+
+interface FormState {
+  success: boolean
+  message: string | null
+  errors: Record<string, string[]> | null
+}
+
+export function useFormState(
+  action: (data: FormData) => Promise<FormState>,
+  initialState?: FormState,
+) {
+  const [isPending, startTransition] = useTransition()
+
+  const [formState, setFormState] = useState(
+    initialState ?? { success: false, message: null, errors: null },
+  )
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const form = event.currentTarget
+    const data = new FormData(form)
+
+    startTransition(async () => {
+      const state = await action(data)
+
+      setFormState(state)
+    })
+  }
+
+  return [formState, handleSubmit, isPending] as const
+}
