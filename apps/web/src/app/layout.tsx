cdb47944a873e423c974ec083380e540a60389b6
Vini Fraga <vinifragam@gmail.com>
2024-06-04 11:43:57 -0300

    Desenvolvendo o front-end - Setup do shadcn/ui

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>{children}</body>
     </html>
   )
