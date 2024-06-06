a85e5c2d0676384b1824cedcc94763e7476771d2
Vini Fraga <vinifragam@gmail.com>
2024-06-06 09:16:54 -0300

    Desenvolvendo o front-end - Componente: organization-switcher

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,7 +1,9 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
 
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
@@ -13,6 +15,10 @@ export function Header() {
           className="size-6 dark:invert"
           alt="Rocketseat"
         />
+
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
