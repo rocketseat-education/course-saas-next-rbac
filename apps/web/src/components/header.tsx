dfedb630275e111c5e27a6107a5d31bbd20f7c2c
Vini Fraga <vinifragam@gmail.com>
2024-06-06 12:29:55 -0300

    Desenvolvendo o front-end - Temas dark/light com Next Themes

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -6,6 +6,8 @@ import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
+import { ThemeSwitcher } from './theme/theme-switcher'
+import { Separator } from './ui/separator'
 
 export async function Header() {
   const permissions = await ability()
@@ -27,6 +29,8 @@ export async function Header() {
       </div>
 
       <div className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
       </div>
     </div>
