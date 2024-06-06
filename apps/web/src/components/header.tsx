93974c879441ccff598606c3d2076d6935027561
Vini Fraga <vinifragam@gmail.com>
2024-06-06 12:13:30 -0300

    Desenvolvendo o front-end - Obtendo permissões do usuário

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,11 +2,14 @@ import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
-export function Header() {
+export async function Header() {
+  const permissions = await ability()
+
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
@@ -19,6 +22,8 @@ export function Header() {
         <Slash className="size-3 -rotate-[24deg] text-border" />
 
         <OrganizationSwitcher />
+
+        {permissions?.can('get', 'Project') && <p>Project</p>}
       </div>
 
       <div className="flex items-center gap-4">
