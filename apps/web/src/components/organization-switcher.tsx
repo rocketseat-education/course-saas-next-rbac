82f17d9d02689d1086f4acf9945a9c0f79ed6968
Vini Fraga <vinifragam@gmail.com>
2024-06-06 09:16:54 -0300

    Desenvolvendo o front-end - Componente: organization-switcher

---
+++ apps/web/src/components/organization-switcher.tsx
@@ -0,0 +1,48 @@
+import { ChevronsUpDown, PlusCircle } from 'lucide-react'
+import Link from 'next/link'
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function OrganizationSwitcher() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
+        <span className="text-muted-foreground">Select organization</span>
+        <ChevronsUpDown className="ml-auto size-4 text-muted-foreground" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        alignOffset={-16}
+        sideOffset={12}
+        className="w-[200px]"
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Organizations</DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Avatar className="mr-2 size-4">
+              <AvatarImage src="https://github.com/rocketseat.png" />
+              <AvatarFallback />
+            </Avatar>
+            <span className="line-clamp-1">Rocketseat</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/create-organization">
+            <PlusCircle className="mr-2 size-4" />
+            Create new
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
