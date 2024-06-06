3b0f34596092688bdc6f4aae30c53da7f5c170d3
Vini Fraga <vinifragam@gmail.com>
2024-06-06 09:27:18 -0300

    Desenvolvendo o front-end - Listando organizações

---
+++ apps/web/src/http/get-organizations.ts
@@ -0,0 +1,16 @@
+import { api } from './api-client'
+
+interface GetOrganizationsResponse {
+  organizations: {
+    id: string
+    name: string
+    slug: string
+    avatarUrl: string | null
+  }[]
+}
+
+export async function getOrganizations() {
+  const result = await api.get('organizations').json<GetOrganizationsResponse>()
+
+  return result
+}
