56a2dceb04465e709efca53c6de4e42be201b961
Vini Fraga <vinifragam@gmail.com>
2024-07-17 11:56:54 -0300

    Desenvolvendo o front-end - Permissão para criar projeto

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -1,8 +1,17 @@
+import { redirect } from 'next/navigation'
+
+import { ability } from '@/auth/auth'
 import { Header } from '@/components/header'
 
 import { ProjectForm } from './project-form'
 
-export default function CreateProject() {
+export default async function CreateProject() {
+  const permissions = await ability()
+
+  if (permissions?.cannot('create', 'Project')) {
+    redirect('/')
+  }
+
   return (
     <div className="space-y-4 py-4">
       <Header />
