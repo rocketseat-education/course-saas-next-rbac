6e5f587475a8668020fc9a2c333ee95531b6c709
Vini Fraga <vinifragam@gmail.com>
2024-09-12 14:55:17 -0300

    Deploy - Deploy do back-end

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -115,6 +115,6 @@ app.register(getPendingInvites)
 
 app.register(getOrganizationBilling)
 
-app.listen({ port: env.SERVER_PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
--- packages/env/index.ts
+++ packages/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    SERVER_PORT: z.coerce.number().default(3333),
+    PORT: z.coerce.number().default(3333),
     DATABASE_URL: z.string().url(),
 
     JWT_SECRET: z.string(),
@@ -17,7 +17,7 @@ export const env = createEnv({
     NEXT_PUBLIC_API_URL: z.string().url(),
   },
   runtimeEnv: {
-    SERVER_PORT: process.env.SERVER_PORT,
+    PORT: process.env.SERVER_PORT,
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
